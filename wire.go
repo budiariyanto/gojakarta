dc5ff17f2731e84f891f2ccbf159f156d29e2d02
Budi Ariyanto <budi@kitabisa.com>
2020-02-20 17:22:39 +0700

    Wire with set

--- wire.go
+++ wire.go
@@ -8,6 +8,12 @@ import (
 	"github.com/google/wire"
 )
 
+// var otpRepoBinding = wire.Bind(new(repository.OTPRepoer), new(*repository.OTPRepo))
+// var otpValidatorBinding = wire.Bind(new(service.OTPValidator), new(*service.Validator))
+
+var manWoman = wire.NewSet(NewMan, NewWoman)
+
 func InitializeMan(ctx context.Context, manName string, wcfg WomanConfig) *Man {
-	panic(wire.Build(NewMan, NewWoman))
+	// panic(wire.Build(NewMan, NewWoman))
+	panic(wire.Build(manWoman))
 }
